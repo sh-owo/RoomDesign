d24ecd5da05e2b6138ef6c5ccbc8e0b27b710081
SHI SHENGZE <ciszen0131@gmail.com>
2024-11-22 10:01:08 +0900

    UI 완성

--- Assets/Script/UI/UI_Change.cs
+++ Assets/Script/UI/UI_Change.cs
@@ -3,8 +3,26 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_Change : MonoBehaviour
-{
-    public List<Sprite> spriteList;
+{   
+    private List<Sprite> spriteList = new List<Sprite>();
+
+    // frame1 ~ frame5 스프라이트를 UI assets 폴더에서 로드
+    void Awake()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Sprite sprite = Resources.Load<Sprite>($"ui assets/UI{i}");
+            if (sprite != null)
+            {
+                spriteList.Add(sprite);
+            }
+            else
+            {
+                Debug.LogWarning($"ui assets/Property {i}=Default 스프라이트를 찾을 수 없습니다!");
+            }
+        }
+    }
+
     public Image uiImage; // 변경할 UI 이미지
     public Sprite variant2Sprite; // X키를 누르면 표시될 이미지
 
@@ -21,7 +39,13 @@ public class UI_Change : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.X))
         {
-            uiImage.sprite = spriteList[index++];
+            if (++index >= spriteList.Count) index = 0;
+            uiImage.sprite = spriteList[index];
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if(--index < 0) index = spriteList.Count - 1;
+            uiImage.sprite = spriteList[index];
         }
     }
 }
